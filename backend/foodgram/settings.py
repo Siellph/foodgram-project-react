dcc0d1f25d4df2b29ef57f2fbcaed29840f0922a
Vladislav <vl241095@gmail.com>
2023-03-29 16:05:38 +0500

    v.0.1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ def get_list_allowed(allowed: str) -> list:
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='your_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=True)
 
@@ -68,14 +68,21 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME', default='postgres'),
+#         'USER': os.getenv('POSTGRES_USER', default='postgres'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+#         'HOST': os.getenv('DB_HOST', default='db'),
+#         'PORT': os.getenv('DB_PORT', default='5432')
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
---
+++ backend/recipes/management/commands/upload_data.py
@@ -0,0 +1,19 @@
+import csv
+from django.core.management import BaseCommand
+from recipes.models import Ingredient
+
+MODELS_FILES = {
+    Ingredient: 'ingredients.csv',
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for model, file in MODELS_FILES.items():
+            with open(f'../data/{file}', encoding='utf-8') as table:
+                reader = csv.DictReader(table)
+                model.objects.bulk_create(model(**data) for data in reader)
+            self.stdout.write(self.style.SUCCESS(
+                f'=== {file} успешно загружен ===')
+            )
+        self.stdout.write(self.style.SUCCESS('=== Загрузка завершена ==='))
