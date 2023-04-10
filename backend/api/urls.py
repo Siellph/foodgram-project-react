52f0846c7265db9bd8fa7e997f8b1ae5b147637b
Vladislav <vl241095@gmail.com>
2023-04-10 16:29:03 +0500

    fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,6 @@ from api.views.users import UsersViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-app_name = 'api'
-
 router = DefaultRouter()
 router.register(r'users', UsersViewSet)
 router.register(r'ingredients', IngredientViewSet)
