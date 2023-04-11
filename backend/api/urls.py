ff69a21ddf51474b4ca60a35bf7b05fff4647318
Vladislav <vl241095@gmail.com>
2023-04-11 15:35:07 +0500

    fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,19 @@
 from api.views.recipes import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views.base_api import BaseAPIRootView
 from api.views.users import UsersViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+app_name = 'api'
+
+
+class RuDefaultRouter(DefaultRouter):
+    """Показывает описание главной страницы API на русском языке.
+    """
+    APIRootView = BaseAPIRootView
+
+
+router = RuDefaultRouter()
 router.register(r'users', UsersViewSet)
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'recipes', RecipeViewSet)
---
+++ backend/api/views/base_api.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import APIRootView
+
+
+class BaseAPIRootView(APIRootView):
+    """Базовые пути API приложения.
+    """
