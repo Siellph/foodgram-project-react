83af3d01a34d04fd80119a95ad5d7efc02d79498
Liam RB <lieam.rb@gmail.com>
2023-04-11 21:08:53 +0500

    fix readme

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='your_secret_key')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = get_list_allowed(
     os.getenv('ALLOWED_HOSTS', default='localhost')
